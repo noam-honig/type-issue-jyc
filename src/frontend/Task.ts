52be4e1a63f61633b25c49d34411c5f9d6e2a293
Noam <noam@fireflymigration.com>
2024-03-14 19:46:44 +0200

    added validations

--- src/frontend/Task.ts
+++ src/frontend/Task.ts
@@ -1,6 +1,20 @@
+import { Entity, Fields, Validators } from "remult"
+
+@Entity("asdfasd")
 export class Task {
   id = ''
+  @Fields.string({ validate: [Validators.unique] })
   title = ''
+
   completed = false
   createdAt?: Date
+
+
+  @Fields.string({ allowNull: true, validate: [Validators.unique] })
+  nom2?: string
+  // => Type 'Validator<any>' is not assignable to type 'FieldValidator<ValidationMessage<any, undefined> | undefined, string>' ...
+
+  // if I do:
+  @Fields.string({ allowNull: true, validate: [Validators.unique("Has to be Unique")] })
+  nom3?: string
 }
